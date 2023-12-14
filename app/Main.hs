7f9e01fd32db998d2fb2a82b83286c1da44ac98e
Josh Brown <joshcarlbrown@gmail.com>
2023-12-15 09:11:05 +1100

    add welcome message

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,21 @@ import Calculator
 import Control.Monad
 import System.IO
 
+welcomeString :: String
+welcomeString =
+    " _                      _       _             \n\
+    \| |                    | |     | |            \n\
+    \| |__   __ _  ___ _   _| | __ _| |_ ___  _ __ \n\
+    \| '_ \\ / _` |/ __| | | | |/ _` | __/ _ \\| '__|\n\
+    \| | | | (_| | (__| |_| | | (_| | || (_) | |   \n\
+    \|_| |_|\\__,_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|\n\n"
+
 main :: IO ()
-main = forever $ do
-    putStr "> "
-    hFlush stdout
-    getLine >>= putStrLn . either id show . calculate . filter (/= ' ')
+main =
+    putStr welcomeString
+        >> forever
+            ( do
+                putStr "> "
+                hFlush stdout
+                getLine >>= putStrLn . either id show . calculate . filter (/= ' ')
+            )
