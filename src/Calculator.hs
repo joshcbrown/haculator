814899f44a42840642b2bda79893c8a87e47429c
Josh Brown <joshcarlbrown@gmail.com>
2023-12-14 18:08:10 +1100

    correct binop evaluation
    took a lot of mucking around but pretty happy to have figured that out

--- src/Calculator.hs
+++ src/Calculator.hs
@@ -6,11 +6,11 @@ data BinOp = Add | Subtract | Multiply | Divide
 data Expr = Constant Rational | BinOp BinOp Expr Expr | Negate Expr
     deriving (Show)
 
-eval :: Expr -> Rational
-eval (Constant x) = x
-eval (Negate x) = negate (eval x)
-eval (BinOp op x y) = case op of
-    Add -> eval x + eval y
-    Subtract -> eval x - eval y
-    Multiply -> eval x * eval y
-    Divide -> eval x / eval y
+-- eval :: Expr -> Rational
+-- eval (Constant x) = x
+-- eval (Negate x) = negate (eval x)
+-- eval (BinOp op x y) = case op of
+--     Add -> eval x + eval y
+--     Subtract -> eval x - eval y
+--     Multiply -> eval x * eval y
+--     Divide -> eval x / eval y
--- src/Calculator/Parser.hs
+++ src/Calculator/Parser.hs
@@ -4,18 +4,25 @@ module Calculator.Parser where
 
 import Calculator
 import Data.Ratio
-import Data.Text
+import qualified Data.Text as T
 import Data.Void (Void)
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer
 
-type Parser = Parsec Void Text
+type Parser = Parsec Void T.Text
 
 binop :: Parser BinOp -> Parser Expr -> Parser Expr
-binop op newExpr = do
-    a <- newExpr
-    try (BinOp <$> op <*> pure a <*> expr) <|> pure a
+binop op side = do
+    lhs <- side
+    rhs <- many ((,) <$> op <*> side)
+    pure
+        $ if null rhs
+            then lhs
+            else
+                let (op1, rhs1) = head rhs
+                    start = BinOp op1 lhs rhs1
+                 in foldr (\(newOp, newRhs) t -> BinOp newOp t newRhs) start (tail rhs)
 
 add :: Parser Expr
 add = binop (Add <$ char '+' <|> Subtract <$ char '-') mult <|> escaped
