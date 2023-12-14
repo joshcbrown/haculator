8c63c7a29ddce22fcf592cf5cf01c31280319a4e
Josh Brown <joshcarlbrown@gmail.com>
2023-12-15 07:51:20 +1100

    fix multiple bugs in parser
    this is really messy now, gonna have a think about how to tidy up

--- src/Calculator.hs
+++ src/Calculator.hs
@@ -1,16 +1,19 @@
 module Calculator where
 
-data BinOp = Add | Subtract | Multiply | Divide
-    deriving (Show)
+import Calculator.Parser
+import Control.Arrow (ArrowChoice (left))
+import qualified Data.Text as T
+import Data.Void (Void)
+import Text.Megaparsec (ParseErrorBundle, errorBundlePretty, runParser)
 
-data Expr = Constant Rational | BinOp BinOp Expr Expr | Negate Expr
-    deriving (Show)
+eval :: Expr -> Rational
+eval (Constant x) = x
+eval (Negate x) = negate (eval x)
+eval (BinOp op x y) = case op of
+    Add -> eval x + eval y
+    Subtract -> eval x - eval y
+    Multiply -> eval x * eval y
+    Divide -> eval x / eval y
 
--- eval :: Expr -> Rational
--- eval (Constant x) = x
--- eval (Negate x) = negate (eval x)
--- eval (BinOp op x y) = case op of
---     Add -> eval x + eval y
---     Subtract -> eval x - eval y
---     Multiply -> eval x * eval y
---     Divide -> eval x / eval y
+calculate :: (Fractional a) => String -> Either String a
+calculate = left errorBundlePretty . fmap (fromRational . eval) . runParser full "input" . T.pack
--- src/Calculator/Parser.hs
+++ src/Calculator/Parser.hs
@@ -1,46 +1,57 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Calculator.Parser where
 
-import Calculator
+import Data.Bool (bool)
+import qualified Data.Map as M
 import Data.Ratio
 import qualified Data.Text as T
 import Data.Void (Void)
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer
+import Text.Megaparsec.Debug
+
+data BinOp = Add | Subtract | Multiply | Divide
+    deriving (Show, Eq, Ord)
+
+data Expr = Constant Rational | BinOp BinOp Expr Expr | Negate Expr
+    deriving (Show)
 
 type Parser = Parsec Void T.Text
 
-binop :: Parser BinOp -> Parser Expr -> Parser Expr
-binop op side = do
-    lhs <- side
-    rhs <- many ((,) <$> op <*> side)
-    pure
-        $ if null rhs
-            then lhs
-            else
-                let (op1, rhs1) = head rhs
-                    start = BinOp op1 lhs rhs1
-                 in foldr (\(newOp, newRhs) t -> BinOp newOp t newRhs) start (tail rhs)
+
+
+
+binop :: Parser BinOp -> Parser Expr -> Bool -> Parser Expr
+binop op side bail = do
+    lhs <- dbg "lhs" side
+    rhs <- dbg "rhs" $ many ((,) <$> op <*> side)
+    if null rhs
+        then if bail then fail "unable to find" else pure lhs
+        else
+            let (op1, rhs1) = head rhs
+                start = BinOp op1 lhs rhs1
+             in pure $ foldr (\(newOp, newRhs) t -> BinOp newOp t newRhs) start (tail rhs)
 
 add :: Parser Expr
-add = binop (Add <$ char '+' <|> Subtract <$ char '-') mult <|> escaped
+add = dbg "add" $ binop (Add <$ char '+' <|> Subtract <$ char '-') (mult <|> escaped) True
 
 mult :: Parser Expr
-mult = binop (Multiply <$ char '*' <|> Divide <$ char '/') escaped
+mult = dbg "mult" $ binop (Multiply <$ char '*' <|> Divide <$ char '/') escaped False
 
 negative :: Parser Expr
-negative = Negate <$> (char '-' *> escaped)
+negative = dbg "neg" $ Negate <$> (char '-' *> escaped)
 
 number :: Parser Expr
-number = Constant . toRational <$> (try float <|> decimal)
+number = dbg "num" $ Constant . toRational <$> (try float <|> decimal)
 
 escaped :: Parser Expr
-escaped = choice [bracketed, negative, number]
+escaped = choice [negative, bracketed, number]
 
 bracketed :: Parser Expr
-bracketed = between (char '(') (char ')') expr
+bracketed = dbg "brackets" $ between (char '(') (char ')') expr
 
 expr :: Parser Expr
-expr = choice [add, mult, bracketed, escaped]
+expr = try add <|> mult
+
+full :: Parser Expr
+full = dbg "full" $ expr <* eof
