754c2dc46f863c496dd0a6cd72615952da518fc1
Josh Brown <joshcarlbrown@gmail.com>
2023-12-21 17:00:29 +1100

    make parser generic
    ideas somewhat shamelessly taken from "Design Patterns for Parser Combinators"
    https://dl.acm.org/doi/10.1145/3471874.3472984
    https://github.com/j-mie6/design-patterns-for-parser-combinators

--- src/Calculator.hs
+++ src/Calculator.hs
@@ -1,25 +1,35 @@
 module Calculator where
 
-import Calculator.Parser
 import Control.Arrow (ArrowChoice (left, right))
 import Control.Exception (ArithException (DivideByZero), catch, try)
 import Control.Monad (join)
 import Data.Bifunctor (Bifunctor (bimap))
 import qualified Data.Text as T
 import Data.Void (Void)
+import Parser.AST
 import Text.Megaparsec (ParseErrorBundle, errorBundlePretty, runParser)
 
-eval :: Expr -> Either ArithException Rational
-eval (Number x) = Right x
-eval (Negate x) = negate <$> eval x
-eval (BinOp op x y) = case op of
-    Add -> (+) <$> eval x <*> eval y
-    Subtract -> (-) <$> eval x <*> eval y
-    Multiply -> (*) <$> eval x <*> eval y
-    Divide -> case eval y of
+evalAtom :: Atom -> Either ArithException Rational
+evalAtom (Number x) = Right x
+evalAtom (Parens e) = eval e
+
+evalNegate :: Negate -> Either ArithException Rational
+evalNegate (Neg n) = negate <$> evalNegate n
+evalNegate (OfAtom a) = evalAtom a
+
+evalMult :: Mult -> Either ArithException Rational
+evalMult (Multiply m n) = (*) <$> evalMult m <*> evalNegate n
+evalMult (Divide m n) =
+    case evalNegate n of
         e@(Left _) -> e
         (Right 0) -> Left DivideByZero
-        (Right ey) -> (/ ey) <$> eval x
+        (Right x) -> (/ x) <$> evalMult m
+evalMult (OfNegate n) = evalNegate n
+
+eval :: Expr -> Either ArithException Rational
+eval (Add e m) = (+) <$> eval e <*> evalMult m
+eval (Subtract e m) = (-) <$> eval e <*> evalMult m
+eval (OfMult m) = evalMult m
 
 calculate :: (Fractional a) => String -> Either String a
 calculate =
