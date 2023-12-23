c3cd99bbb4bc84bb3968f71c4bb12408b10564cf
Josh Brown <joshcarlbrown@gmail.com>
2023-12-23 16:35:55 +1100

    improve error handling

--- src/Calculator.hs
+++ src/Calculator.hs
@@ -2,12 +2,12 @@
 
 module Calculator where
 
-import Control.Arrow (Arrow (second), ArrowChoice (left))
+import Control.Arrow (ArrowChoice (left))
 import Control.Exception (ArithException (..))
 import Control.Monad (join)
 import Data.Bifunctor (Bifunctor (bimap))
 import qualified Data.Map as M
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromJust, fromMaybe)
 import qualified Data.Text as T
 import Parser.AST
 import Text.Megaparsec (errorBundlePretty, runParser)
@@ -16,16 +16,18 @@ data Term = Constant | Var String
     deriving (Show, Eq, Ord)
 
 type Added = M.Map Term Rational
+data EvalErr = Arith ArithException | NonLinear | NoVariable | TooManyVars [Term]
+    deriving (Show)
 
-type EvalResult = Either ArithException Added
+type EvalResult a = Either EvalErr a
 type Solution a = (Term, a)
 
-evalAtom :: Atom -> EvalResult
+evalAtom :: Atom -> EvalResult Added
 evalAtom (Number x) = Right $ M.singleton Constant x
 evalAtom (Parens e) = eval e
 evalAtom (Ident s) = Right $ M.singleton (Var s) 1
 
-evalNegate :: Negate -> EvalResult
+evalNegate :: Negate -> EvalResult Added
 evalNegate (Neg n) = fmap negate <$> evalNegate n
 evalNegate (OfAtom a) = evalAtom a
 
@@ -37,28 +39,30 @@ containsVar :: Added -> Bool
 containsVar m = any isVar (M.keys m)
 
 -- horrendously ugly but i think it works
-multiply :: Added -> Added -> EvalResult
-multiply m1 m2 = M.foldrWithKey (\k v -> (multiplyEach k v =<<)) (Right M.empty) m1
+multiply :: Added -> Added -> EvalResult Added
+multiply m1 m2 = M.foldrWithKey (\k v -> (foilIter k v =<<)) (Right M.empty) m1
   where
-    multiplyEach k v m = M.unionWith (+) m <$> new k v
-    new k v = case k of
-        Constant -> Right $ (* v) <$> m2
-        Var s -> if containsVar m2 then Left Denormal else Right $ M.singleton (Var s) (v * constVal)
-    -- TODO: should really error here if there's no constant
-    constVal = fromMaybe 1 (M.lookup Constant m2)
+    foilIter k v m = M.unionWith (+) m <$> multiplyEach k v
+    multiplyEach k v = case k of
+        Constant -> Right ((* v) <$> m2)
+        Var s ->
+            if containsVar m2
+                then Left NonLinear
+                else Right $ M.singleton (Var s) (v * constVal)
+    constVal = fromJust (M.lookup Constant m2)
 
-evalMult :: Mult -> EvalResult
+evalMult :: Mult -> EvalResult Added
 evalMult (Multiply m n) = join $ multiply <$> evalMult m <*> evalNegate n
 evalMult (Divide m n) =
     case evalNegate n of
         e@(Left _) -> e
         (Right x) -> case M.lookup Constant x of
-            Just 0 -> Left RatioZeroDenominator
+            Just 0 -> Left $ Arith RatioZeroDenominator
             Just y -> fmap (/ y) <$> evalMult m
-            Nothing -> Left Denormal
+            Nothing -> Left NonLinear
 evalMult (OfNegate n) = evalNegate n
 
-eval :: Expr -> EvalResult
+eval :: Expr -> EvalResult Added
 eval (Add e m) = M.unionWith (+) <$> eval e <*> evalMult m
 eval (Subtract e m) = unionWithDefault (-) 0 <$> eval e <*> evalMult m
 eval (OfMult m) = evalMult m
@@ -66,20 +70,22 @@ eval (OfMult m) = evalMult m
 getConst :: Added -> Rational
 getConst m = fromMaybe 0 (M.lookup Constant m)
 
-getVar :: Added -> Maybe (Term, Rational)
-getVar m = if length ts /= 1 then Nothing else Just $ head ts
+getVar :: Added -> EvalResult (Term, Rational)
+getVar m =
+    case length ts of
+        0 -> Left NoVariable
+        1 -> Right $ head ts
+        _ -> Left (TooManyVars (map fst ts))
   where
     ts = filter (isVar . fst) (M.toList m)
 
-solve :: Equation -> Either ArithException (Solution Rational)
+solve :: Equation -> EvalResult (Solution Rational)
 solve (Equate e1 e2) = do
     m1 <- eval e1
     m2 <- eval e2
-    let lhs = getVar $ unionWithDefault (-) 0 m1 m2
-        rhs = getConst $ unionWithDefault (-) 0 m2 m1
-    case lhs of
-        Nothing -> Left Denormal
-        Just (name, coef) -> pure (name, rhs / coef)
+    (name, coef) <- getVar $ unionWithDefault (-) 0 m1 m2
+    let rhs = getConst $ unionWithDefault (-) 0 m2 m1
+    if coef == 0 then Left NoVariable else pure (name, rhs / coef)
 
 unionWithDefault :: (Ord k) => (v -> v -> v) -> v -> M.Map k v -> M.Map k v -> M.Map k v
 unionWithDefault f defaultv m1 m2 = M.unionWith f m1' m2'
